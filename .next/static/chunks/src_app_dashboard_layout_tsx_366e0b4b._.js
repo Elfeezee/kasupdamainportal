1c1a69a0f093dc4f32733aec555d819bfc69ee6e
Muhammad Kabiru Rabiu <muhammadkabirmk52@gmail.com>
2025-11-03 09:30:16 +0000

    when the user login to his dashboard, i want it to hide entirely the hea

---
+++ .next/static/chunks/src_app_dashboard_layout_tsx_366e0b4b._.js
@@ -0,0 +1,13 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/src_app_dashboard_layout_tsx_366e0b4b._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/src_app_dashboard_layout_tsx_366e0b4b._.js",
+  "chunks": [
+    "static/chunks/node_modules_@supabase_node-fetch_browser_3ae92900.js",
+    "static/chunks/node_modules_7f9440a0._.js",
+    "static/chunks/src_c53279ed._.js"
+  ],
+  "source": "dynamic"
+});
---
+++ .next/static/chunks/src_app_login_page_tsx_366e0b4b._.js
@@ -0,0 +1,15 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/src_app_login_page_tsx_366e0b4b._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/src_app_login_page_tsx_366e0b4b._.js",
+  "chunks": [
+    "static/chunks/node_modules_@supabase_node-fetch_browser_d3cc5d48.js",
+    "static/chunks/node_modules_react-icons_e0321124._.js",
+    "static/chunks/node_modules_@supabase_auth-js_dist_module_6004bf20._.js",
+    "static/chunks/node_modules_b5271784._.js",
+    "static/chunks/src_dad09f50._.js"
+  ],
+  "source": "dynamic"
+});
