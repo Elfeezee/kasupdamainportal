17f6cf2646e81c581ecd4f8c12540b13a2184e0b
Muhammad Kabiru Rabiu <muhammadkabirmk52@gmail.com>
2025-11-05 23:40:07 +0000

    see the print out, please for the last time, let it shown in the printou

--- src/app/dashboard/acknowledgement/[id]/page.tsx
+++ src/app/dashboard/acknowledgement/[id]/page.tsx
@@ -52,7 +52,7 @@ export default async function AcknowledgementPage({ params }: { params: { id: st
     const applicationData = await getApplicationData(params.id);
     
     return (
-        <div className="container mx-auto px-2 sm:px-4 py-8 bg-gray-200 dark:bg-gray-900 print:bg-white print-container">
+        <div className="print-container">
              <Suspense fallback={<AcknowledgementLoadingSkeleton />}>
                 <AcknowledgementLetterContent applicationData={applicationData} />
             </Suspense>
