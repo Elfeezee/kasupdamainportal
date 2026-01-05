05d23b779b3073db280f91f469dacb4522b211b0
Muhammad Kabiru Rabiu <muhammadkabirmk52@gmail.com>
2026-01-05 10:39:57 +0000

    /favicon.ico:1  Failed to load resource: the server responded with a sta

--- src/app/dashboard/apply/commercial-industrial-other-permit/page.tsx
+++ src/app/dashboard/apply/commercial-industrial-other-permit/page.tsx
@@ -764,7 +764,7 @@ function CheckIcon(props: React.SVGProps<SVGSVGElement>) {
       xmlns="http://www.w3.org/2000/svg"
       width="24"
       height="24"
-      viewBox="0 0 24"
+      viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
       strokeWidth="2"
